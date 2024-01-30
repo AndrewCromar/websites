0e5b17d54eff2f08338a393878a4944ec7b5bc4c
Andrew Cromar <andrewmcromar@gmail.com>
2024-01-30 13:53:03 -0800

    added programming page

---
+++ scripts/page_setup.js
@@ -0,0 +1,18 @@
+function setup_page(title){
+    document.write(`
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/sticky_navbar.css">
+
+    <div class="title-area">
+        <center><h1>${title}<h1></center>
+        <title>Andrew Cromar</title>
+    </div>
+
+    <div class="navbar">
+        <a class="nav-button" href="index.html">Home</a>
+        <a class="nav-button" href="programming.html">Programming</a>
+    </div>
+
+    <script src="scripts/sticky_navbar.js"></script>
+`);
+}
