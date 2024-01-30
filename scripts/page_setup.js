ba6c3ac72a943b56c23c03ab3d655ba227ece4dc
Andrew Cromar <andrewmcromar@gmail.com>
2024-01-30 14:56:46 -0800

    added resume page and files for the page

--- scripts/page_setup.js
+++ scripts/page_setup.js
@@ -1,6 +1,7 @@
 function setup_page(title){
     document.write(`
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/resume.css">
     <link rel="stylesheet" href="styles/sticky_navbar.css">
 
     <div class="title-area">
@@ -11,6 +12,7 @@ function setup_page(title){
     <div class="navbar">
         <a class="nav-button" href="index.html">Home</a>
         <a class="nav-button" href="programming.html">Programming</a>
+        <a class="nav-button" href="Resume.html">Resume</a>
     </div>
 
     <script src="scripts/sticky_navbar.js"></script>
