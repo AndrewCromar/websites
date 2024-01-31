2cf23f6b7a3cd1031edc9016a8f5483875bdee36
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-01-30 19:17:41 -0800

    tilt

--- scripts/tilt.js
+++ scripts/tilt.js
@@ -47,10 +47,6 @@ el.addEventListener('mouseout', function() {
   el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
 })
 
-el.addEventListener('mousedown', function() {
-  el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
-})
-
 /* Add listener for mouseup, simulate release of mouse click */
 el.addEventListener('mouseup', function() {
   el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
