560c624b8dff39be8bfa39dada2128a766600e0f
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-01-30 20:16:08 -0800

    changed target to blank for social links

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,7 +2,7 @@ document.write(`
     <script src="scripts/sticky_navbar.js"></script>
 
     <div class="footer">
-        <a class="social" href="https://github.com/AndrewCromar">GitHub</a>
-        <a class="social" href="https://github.com/AndrewCromar">GitHub</a>
+        <a class="social" target="_blank" href="https://github.com/AndrewCromar">GitHub</a>
+        <a class="social" target="_blank" href="https://github.com/AndrewCromar">GitHub</a>
     </div>
 `);
