0a1e4c87ed61361d7944117fc38a8dbb8ab678c9
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-01-30 20:32:21 -0800

    colors

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,7 +2,7 @@ document.write(`
     <script src="scripts/sticky_navbar.js"></script>
 
     <div class="footer">
-        <a class="social" target="_blank" href="https://github.com/AndrewCromar">GitHub</a>
-        <a class="social" target="_blank" href="https://www.youtube.com/@ONYXDevelopment">Youtube</a>
+        <a class="social github" target="_blank" href="https://github.com/AndrewCromar">GitHub</a>
+        <a class="social youtube" target="_blank" href="https://www.youtube.com/@ONYXDevelopment">Youtube</a>
     </div>
 `);
