74a7f0dae6e1234e48c1276fc3385b5e1b7aba92
Andrew Cromar <andrewmcromar@gmail.com>
2024-01-30 20:55:48 -0800

    changed footer and make nice looking social buttons

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,7 +2,8 @@ document.write(`
     <script src="scripts/sticky_navbar.js"></script>
 
     <div class="footer">
-        <a class="social github" target="_blank" href="https://github.com/AndrewCromar">GitHub</a>
-        <a class="social youtube" target="_blank" href="https://www.youtube.com/@ONYXDevelopment">Youtube</a>
+        <a class="social github" target="_blank" href="https://github.com/AndrewCromar"><img src="images/github_text.png"></a>
+        <a class="social youtube" target="_blank" href="https://www.youtube.com/@ONYXDevelopment"><img src="images/youtube_text.png"></a>
+        <a class="social linkedin" target="_blank" href="https://www.linkedin.com/in/andrew-cromar-5257b22ab/"><img src="images/linkedin_text.png"></a>
     </div>
 `);
