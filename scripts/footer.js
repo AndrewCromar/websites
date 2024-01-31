5171a608e4f64e5617f1e8ae8cf1363bc0fb5017
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-01-30 20:12:02 -0800

    footer stuff

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,6 +2,7 @@ document.write(`
     <script src="scripts/sticky_navbar.js"></script>
 
     <div class="footer">
-        <p>test</p>
+        <p>https://www.andrewcromar.org/</p>
+        <a class="social" href="https://github.com/AndrewCromar">GitHub</a>
     </div>
 `);
