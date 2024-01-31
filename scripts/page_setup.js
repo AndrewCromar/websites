1837354187b12f1c45f523d0a014a7a2b3b25a55
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2024-01-30 19:46:07 -0800

    css

--- scripts/page_setup.js
+++ scripts/page_setup.js
@@ -3,6 +3,7 @@ function setup_page(title){
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/resume.css">
     <link rel="stylesheet" href="styles/sticky_navbar.css">
+    <link rel="stylesheet" href="styles/social.css">
 
     <div class="header">
         <center><h1>${title}<h1></center>
