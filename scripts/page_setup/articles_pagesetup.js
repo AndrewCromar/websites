986d8900a163da6c32ba7f868e018e296f85cc01
Andrew Cromar <andrewmcromar@gmail.com>
2024-02-24 21:15:12 -0800

    added slide transition and made it work with reduced motion

--- scripts/page_setup/articles_pagesetup.js
+++ scripts/page_setup/articles_pagesetup.js
@@ -2,16 +2,19 @@ function Header(title, icon_path){
     document.write(`
         <link rel="stylesheet" href="../../styles/style.css">
         <link rel="stylesheet" href="../../styles/sticky_navbar.css">
-        
+
+        <script src="../../scripts/loading_transition.js"></script>
+
         <div class="header">
             <center><h1 style="font-weight: bold;">${title}</h1></center>
             <title>Andrew Cromar</title>
         </div>
         
         <div id="navbar">
-            <a class="nav-button" href="../andrewcromar/home.html">andrewcromar.org</a>
-            <a class="nav-button" href="articles.html">Articles</a>
-            <a class="nav-button" href="less-homework-relates-to-greater-learning.html">LHRGL</a>
+            <a class="nav-button" onclick="navigateWithLoadingScreen(event, '../andrewcromar/home.html')">andrewcromar.org</a>
+            <a class="nav-button" onclick="navigateWithLoadingScreen(event, 'articles.html')">Articles</a>
+            <a class="nav-button" onclick="navigateWithLoadingScreen(event, 'less-homework-relates-to-greater-learning.html')">LHRGL</a>
+
             <img class="icon" src="${icon_path}">
         </div>
     `);
