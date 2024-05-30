8090e2f0aa4f4f662d6e38dab7e9c70e1f12f337
Andrew Cromar <andrewmcromar@gmail.com>
2024-05-30 09:49:18 -0700

    reordered top button things

--- onyx/scripts/totop.js
+++ onyx/scripts/totop.js
@@ -1,7 +1,12 @@
-// Get the button
+// Add the button.
+document.write(`
+    <button onclick="topFunction()" id="topbutton" class="topbutton">Top</button>
+`);
+
+// Get the button.
 let mybutton = document.getElementById("topbutton");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the button.
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,7 +17,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the button, scroll to the top of the document.
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
