2c48ef5fe01ebebe2aa8d8480057614fc104638b
Andrew Cromar <andrewmcromar@gmail.com>
2024-06-26 16:42:21 -0700

    FINALLY GOT THE THEME SWITCHING AND SEARCHING TO WORK!!!

--- themeCommandStuff/scripts/commandline/commands.js
+++ themeCommandStuff/scripts/commandline/commands.js
@@ -1,17 +1,37 @@
 class Command {
-    constructor(id, title, isTheme){
+    constructor(id, title, details, isActive, isTheme, themeColors = []) {
         this.id = id;
         this.title = title;
+        this.details = details;
+        this.isActive = isActive;
         this.isTheme = isTheme;
+        this.themeColors = themeColors;
+        this.clicked = null;
+        this.mouseEnter = null;
+        this.mouseExit = null;
     }
+
+    SetExecutions(clicked, mouseEnter, mouseExit){
+        this.clicked = clicked;
+        this.mouseEnter = mouseEnter;
+        this.mouseExit = mouseExit;
+
+        return this;
+    }
+
+    Clicked() { this.clicked && this.clicked(this); }
+    MouseEnter() { this.mouseEnter && this.mouseEnter(this); }
+    MouseExit() { this.mouseExit && this.mouseExit(this); }
 }
 
 let commands = [];
 
-function MakeCommand(title, isTheme){
-    commands.add(new Command(commands.length, title, isTheme));
+function MakeCommand(title, details, isActive, isTheme, themeColors = []){
+    const newCommand = new Command(commands.length, title, details, isActive, isTheme, themeColors);
+    commands.push(newCommand);
+    return newCommand;
 }
 
-MakeCommand("testCommandOne", false);
-MakeCommand("testCommandTwo", false);
-MakeCommand("themeOne", true);
+// MakeCommand("Theme", "Regular", true, true, ["red", "green", "yellow", "blue"]).SetExecutions(ThemeCommandClicked, ThemeCommandMouseEnter, ThemeCommandMouseExit);
+// MakeCommand("Theme", "Light", false, true, ["red", "green", "yellow", "blue"]).SetExecutions(ThemeCommandClicked, ThemeCommandMouseEnter, ThemeCommandMouseExit);
+// MakeCommand("Theme", "Dark", false, true, ["red", "green", "yellow", "blue"]).SetExecutions(ThemeCommandClicked, ThemeCommandMouseEnter, ThemeCommandMouseExit);
--- themeCommandStuff/scripts/includer.js
+++ themeCommandStuff/scripts/includer.js
@@ -1,5 +1,11 @@
 document.writeln(`
+    <link type="text/css" rel="stylesheet" href="../styles/main.css">
+    
+    <link type="text/css" rel="stylesheet" href="../styles/commandLine.css">
+    
     <script src="../scripts/commandLine/commands.js"></script>
     <script src="../scripts/commandLine/commandLine.js"></script>
-    <link type="text/css" rel="stylesheet" href="../styles/main.css">
+    
+    <script src="../scripts/themeScripts/themes.js"></script>
+    <script src="../scripts/themeScripts/themeController.js"></script>
 `);
