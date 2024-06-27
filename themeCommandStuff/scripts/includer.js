3e0921042b040673ca891f6d1541966cb9844c71
Andrew Cromar <andrewmcromar@gmail.com>
2024-06-26 20:53:29 -0700

    theme management

--- themeCommandStuff/scripts/includer.js
+++ themeCommandStuff/scripts/includer.js
@@ -1,4 +1,9 @@
 document.writeln(`
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
     <link type="text/css" rel="stylesheet" href="../styles/main.css">
     
     <link type="text/css" rel="stylesheet" href="../styles/commandLine.css">
