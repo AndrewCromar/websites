c99dc84b6ca1aa881c4430bbbe8847a375f28b91
Andrew Cromar <andrewmcromar@gmail.com>
2024-07-09 18:02:23 -0700

    command line stuff

--- themeCommandStuff/scripts/commandline/commands.js
+++ themeCommandStuff/scripts/commandline/commands.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 class Command {
     constructor(id, title, details, isActive, isTheme, themeColors = []) {
         this.id = id;
@@ -32,45 +31,3 @@ function MakeCommand(title, details, isActive, isTheme, themeColors = []){
     commands.push(newCommand);
     return newCommand;
 }
-
-// MakeCommand("Theme", "Regular", true, true, ["red", "green", "yellow", "blue"]).SetExecutions(ThemeCommandClicked, ThemeCommandMouseEnter, ThemeCommandMouseExit);
-// MakeCommand("Theme", "Light", false, true, ["red", "green", "yellow", "blue"]).SetExecutions(ThemeCommandClicked, ThemeCommandMouseEnter, ThemeCommandMouseExit);
-=======
-class Command {
-    constructor(id, title, details, isActive, isTheme, themeColors = []) {
-        this.id = id;
-        this.title = title;
-        this.details = details;
-        this.isActive = isActive;
-        this.isTheme = isTheme;
-        this.themeColors = themeColors;
-        this.clicked = null;
-        this.mouseEnter = null;
-        this.mouseExit = null;
-    }
-
-    SetExecutions(clicked, mouseEnter, mouseExit){
-        this.clicked = clicked;
-        this.mouseEnter = mouseEnter;
-        this.mouseExit = mouseExit;
-
-        return this;
-    }
-
-    Clicked() { this.clicked && this.clicked(this); }
-    MouseEnter() { this.mouseEnter && this.mouseEnter(this); }
-    MouseExit() { this.mouseExit && this.mouseExit(this); }
-}
-
-let commands = [];
-
-function MakeCommand(title, details, isActive, isTheme, themeColors = []){
-    const newCommand = new Command(commands.length, title, details, isActive, isTheme, themeColors);
-    commands.push(newCommand);
-    return newCommand;
-}
-
-// MakeCommand("Theme", "Regular", true, true, ["red", "green", "yellow", "blue"]).SetExecutions(ThemeCommandClicked, ThemeCommandMouseEnter, ThemeCommandMouseExit);
-// MakeCommand("Theme", "Light", false, true, ["red", "green", "yellow", "blue"]).SetExecutions(ThemeCommandClicked, ThemeCommandMouseEnter, ThemeCommandMouseExit);
->>>>>>> Stashed changes
-// MakeCommand("Theme", "Dark", false, true, ["red", "green", "yellow", "blue"]).SetExecutions(ThemeCommandClicked, ThemeCommandMouseEnter, ThemeCommandMouseExit);
