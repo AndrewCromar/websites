ff93d5bcfc3c2ccfa81e474a2914348ca64b7e60
Andrew Cromar <andrewmcromar@gmail.com>
2024-07-20 22:36:09 -0700

    remove : www > onyx > punch her

--- home/scripts/page_setup/onyx_pagesetup.js
+++ home/scripts/page_setup/onyx_pagesetup.js
@@ -15,7 +15,6 @@ function Header(title, icon_path){
             <a class="nav-button" onclick="navigateWithLoadingScreen(event, 'onyx-development.html')">ONYX Development</a>
             <a class="nav-button" onclick="navigateWithLoadingScreen(event, 'hell-hotel.html')">Hell Hotel</a>
             <a class="nav-button" onclick="navigateWithLoadingScreen(event, 'juicy-player-controller.html')">Juicy</a>
-            <a class="nav-button" onclick="navigateWithLoadingScreen(event, 'punch-her.html')">Punch HER</a>
             <!-- <a class="nav-button" onclick="navigateWithLoadingScreen(event, 'ps1-styled-game.html')">PS1 Styled Game</a> -->
 
             <img class="icon" src="${icon_path}">
