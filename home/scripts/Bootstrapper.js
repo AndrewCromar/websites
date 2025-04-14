29fbaed7c2c4c18e114eb208df33d3438cae7036
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-04-14 13:56:15 -0700

    feat(home): update some of the pages

--- home/scripts/Bootstrapper.js
+++ home/scripts/Bootstrapper.js
@@ -17,18 +17,20 @@ function Bootstrapper(_pageTitle, _theme) {
         </div>
         
         <div class="navbar">
-        <div><img src="../images/navbar/home_icon.png" onclick="ToPage('index.html')"></div>
+        <div><img src="../images/icons/home_icon.png" onclick="ToPage('index.html')"></div>
         <hr>
-            <div><img src="../images/navbar/luna_icon.png" onclick="ToPage('luna.html')"></div>
-            <div><img src="../images/navbar/code_icon.png" style="padding: 5px; width: calc(100% - 10px); height: calc(100% - 10px);" onclick="ToPage('languages-and-software.html')"></div>
-            <div><img src="../images/navbar/paper_icon.png" onclick="ToPage('less-homework-relates-to-greater-learing.html')"></div>
-            <div><img src="../images/navbar/clock_icon.png" onclick="ToPage('school-countdown.html')"></div>
+            <div><img src="../images/icons/clock_icon.png" onclick="ToPage('school-countdown.html')"></div>
+            <div><img src="../images/icons/luna_icon.png" onclick="ToPage('luna.html')"></div>
+            <div><img src="../images/icons/code_icon.png" style="padding: 5px; width: calc(100% - 10px); height: calc(100% - 10px);" onclick="ToPage('languages-and-software.html')"></div>
+            <div><img src="../images/icons/paper_icon.png" onclick="ToPage('less-homework-relates-to-greater-learing.html')"></div>
             <hr>
-            <div><img src="../images/navbar/onyx_logo.png" onclick="ToPage('https://onyx.andrewcromar.org/')"></div>
-            <div><img src="../images/navbar/trio-production-logo.png" style="padding: 5px; width: calc(100% - 10px); height: calc(100% - 10px);" onclick="ToPage('https://trio.andrewcromar.org/')"></div>
+            <div><img src="../images/icons/onyx_logo.png" onclick="ToPage('https://onyx.andrewcromar.org/')"></div>
+            <div><img src="../images/icons/trio-production-logo.png" style="padding: 5px; width: calc(100% - 10px); height: calc(100% - 10px);" onclick="ToPage('https://trio.andrewcromar.org/')"></div>
             <hr>
-            <div><img src="../images/navbar/github_icon.png" onclick="ToPage('https://github.com/AndrewCromar')"></div>
-            <div><img src="../images/navbar/linkedin_icon.png" onclick="ToPage('https://www.linkedin.com/in/andrew-cromar-5257b22ab/')"></div>
+            <div><img src="../images/icons/github_icon.png" onclick="ToPage('https://github.com/AndrewCromar')"></div>
+            <div><img src="../images/icons/linkedin_icon.png" onclick="ToPage('https://www.linkedin.com/in/andrew-cromar-5257b22ab/')"></div>
+            <hr>
+            <div><img src="../images/icons/settings_icon.png" onclick="ToPage('settings.html')"></div>
         </div>
             
         <script src="../scripts/ToPage.js"></script>
