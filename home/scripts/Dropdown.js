ca9be18bd790246121ba3650b875106304bf28fa
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-04-14 13:55:26 -0700

    feat(home): add dropdown code

---
+++ home/scripts/Dropdown.js
@@ -0,0 +1,19 @@
+document.write(`<link rel="stylesheet" href="../styles/dropdown.css">`);
+
+const dropdownHeaders = document.querySelectorAll('.dropdown-header');
+
+dropdownHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+            content.style.padding = "0 15px";
+            header.classList.remove('open'); // Remove rotation class
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+            content.style.padding = "10px 15px";
+            header.classList.add('open'); // Add rotation class
+        }
+    });
+});
