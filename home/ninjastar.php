3864756173e35ae7cba89cb2535e759371b3dd14
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-06-13 19:08:00 -0700

    rmv(home): email functionality

--- home/ninjastar.php
+++ home/ninjastar.php
@@ -1,5 +1,5 @@
 <?php
-    mail("andrewmcromar@gmail.com", "Ninja Star", "Someone has visited the ninja star page.");
+    // mail("andrewmcromar@gmail.com", "Ninja Star", "Someone has visited the ninja star page.");
 ?>
 
 <p>You are beautiful, don't listen to anyone who says otherwise.</p>
