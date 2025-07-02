427a0763047eae3ecab0de9dc46e39d7467d3617
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-07-01 17:47:48 -0700

    fix(home): studying tagger

--- home/pages/qwertyuiopasdfghjklzxcvbnm.php
+++ home/pages/qwertyuiopasdfghjklzxcvbnm.php
@@ -1,11 +1,11 @@
 <?php
 
-$secret = 'mySuperSecretKey123';
+$secret = 'boobs';
 
 if (!isset($_GET['key']) || $_GET['key'] !== $secret) {
     http_response_code(403);
     exit('Forbidden');
 }
 
-mail('andrewmcromar@email.com', 'Subject', 'Message triggered by GitHub Action');
+mail('andrewmcromar@gmail.com', 'Subject', 'Message triggered by GitHub Action');
 echo 'Email sent.';
