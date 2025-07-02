216c690378f19dcef7930482d4e4fcaa1c83b205
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-07-01 18:18:52 -0700

    upt(home): study tagger

--- home/pages/qwertyuiopasdfghjklzxcvbnm.php
+++ home/pages/qwertyuiopasdfghjklzxcvbnm.php
@@ -1,16 +1,27 @@
 <?php
 
 $secret = 'boobs';
+$use_custom_from = false;
 
 if (!isset($_GET['key']) || $_GET['key'] !== $secret) {
     http_response_code(403);
     exit('Forbidden');
 }
 
-$msg = isset($_GET['msg']) ? $_GET['msg'] : '(No message provided)';
+$msg = isset($_GET['msg']) ? urldecode($_GET['msg']) : '(No message provided)';
 
 $subject = "Andrew studied some college stuff!";
 $body = "Here is what he worked on:\n\n" . $msg;
 
-mail('andrewmcromar@gmail.com', $subject, $body);
+$to = 'andrewmcromar@gmail.com';
+
+if ($use_custom_from) {
+    $from = 'notify@andrewcromar.org';
+    $headers = "From: Andrew Cromar <{$from}>\r\n";
+    $headers .= "Reply-To: {$from}\r\n";
+    mail($to, $subject, $body, $headers);
+} else {
+    mail($to, $subject, $body);
+}
+
 echo 'Email sent.';
