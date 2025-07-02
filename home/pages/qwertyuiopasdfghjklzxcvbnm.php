7aa523e664c8d6679f7c8796ae1fd90f4e37e207
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-07-01 20:37:37 -0700

    test(home): revese what ive done

--- home/pages/qwertyuiopasdfghjklzxcvbnm.php
+++ home/pages/qwertyuiopasdfghjklzxcvbnm.php
@@ -1,17 +1,28 @@
 <?php
 
 $secret = 'boobs';
+$use_custom_from = true;
 
 if (!isset($_GET['key']) || $_GET['key'] !== $secret) {
     http_response_code(403);
     exit('Forbidden');
 }
 
+$msg = isset($_GET['msg']) ? urldecode($_GET['msg']) : '(No message provided)';
+
 $subject = "Andrew studied some college stuff!";
-$body = "Andrew has been studying he just finished a notable bit of research.";
 
-$to = 'andrewmcromar@gmail.com, scromar@gmail.com';
+$body = "Hello Scott,\n\nAndrew has been studying he just finished a notable section.\n\nHere is what he worked on:\n" . $msg;
 
-mail($to, $subject, $body);
+$to = 'andrewmcromar@gmail.com'; //, scromar@gmail.com';
+
+if ($use_custom_from) {
+    $from = 'me@andrewcromar.org';
+    $headers = "From: Automated Andrew <{$from}>\r\n";
+    $headers .= "Reply-To: {$from}\r\n";
+    mail($to, $subject, $body, $headers);
+} else {
+    mail($to, $subject, $body);
+}
 
 echo 'Email sent.';
