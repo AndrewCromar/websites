3c4b6a6fabb146acf68b96eac7a7c74731a1eaf6
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-07-01 20:23:24 -0700

    fix(home): remove custom email

--- home/pages/qwertyuiopasdfghjklzxcvbnm.php
+++ home/pages/qwertyuiopasdfghjklzxcvbnm.php
@@ -1,7 +1,6 @@
 <?php
 
 $secret = 'boobs';
-$use_custom_from = true;
 
 if (!isset($_GET['key']) || $_GET['key'] !== $secret) {
     http_response_code(403);
@@ -13,15 +12,8 @@ $msg = isset($_GET['msg']) ? urldecode($_GET['msg']) : '(No message provided)';
 $subject = "Andrew studied some college stuff!";
 $body = "Here is what he worked on:\n\n" . $msg;
 
-$to = 'andrewmcromar@gmail.com'; //, scromar@gmail.com';
+$to = 'andrewmcromar@gmail.com, scromar@gmail.com';
 
-if ($use_custom_from) {
-    $from = 'me@andrewcromar.org';
-    $headers = "From: Automated Andrew <{$from}>\r\n";
-    $headers .= "Reply-To: {$from}\r\n";
-    mail($to, $subject, $body, $headers);
-} else {
-    mail($to, $subject, $body);
-}
+mail($to, $subject, $body);
 
 echo 'Email sent.';
