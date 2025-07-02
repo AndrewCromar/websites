a5f97be55d94617fbdcce6a49874b9837b89a526
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-07-01 19:51:57 -0700

    test(home): change email

--- home/pages/qwertyuiopasdfghjklzxcvbnm.php
+++ home/pages/qwertyuiopasdfghjklzxcvbnm.php
@@ -1,7 +1,7 @@
 <?php
 
 $secret = 'boobs';
-$use_custom_from = false;
+$use_custom_from = true;
 
 if (!isset($_GET['key']) || $_GET['key'] !== $secret) {
     http_response_code(403);
@@ -16,7 +16,7 @@ $body = "Here is what he worked on:\n\n" . $msg;
 $to = 'andrewmcromar@gmail.com, scromar@gmail.com';
 
 if ($use_custom_from) {
-    $from = 'notify@andrewcromar.org';
+    $from = 'me@andrewcromar.org';
     $headers = "From: Notification <{$from}>\r\n";
     $headers .= "Reply-To: {$from}\r\n";
     mail($to, $subject, $body, $headers);
