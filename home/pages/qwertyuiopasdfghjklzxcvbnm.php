c26752f214d0880732c725b3f274e3a3eb0fc6c7
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-07-01 20:28:18 -0700

    test(home): without scott

--- home/pages/qwertyuiopasdfghjklzxcvbnm.php
+++ home/pages/qwertyuiopasdfghjklzxcvbnm.php
@@ -12,7 +12,7 @@ $msg = isset($_GET['msg']) ? urldecode($_GET['msg']) : '(No message provided)';
 $subject = "Andrew studied some college stuff!";
 $body = "Here is what he worked on:\n\n" . $msg;
 
-$to = 'andrewmcromar@gmail.com, scromar@gmail.com';
+$to = 'andrewmcromar@gmail.com'; //, scromar@gmail.com';
 
 mail($to, $subject, $body);
 
