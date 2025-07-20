3a920bbcce796fc851002fc345c7401faf99462c
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-07-20 16:20:58 -0700

    feat(home): add catherine name to the email

--- home/pages/qwertyuiopasdfghjklzxcvbnm.php
+++ home/pages/qwertyuiopasdfghjklzxcvbnm.php
@@ -15,7 +15,7 @@ $msg = isset($_GET['msg']) ? urldecode($_GET['msg']) : '(No message provided)';
 
 $subject = "Andrew studied some college stuff!";
 
-$body = "Hello Scott,\n\nAndrew has been studying and he just finished a notable section.\n\nHere is what he worked on:\n" . $msg . "\n\nThanks,\nAutomated Andrew\n\nP.S. This is just a commit message; it is supposed to be short.";
+$body = "Hello Scott and Catherine,\n\nAndrew has been studying and he just finished a notable section.\n\nHere is what he worked on:\n" . $msg . "\n\nThanks,\nAutomated Andrew\n\nP.S. This is just a commit message; it is supposed to be short.";
 
 $to = 'scromar@gmail.com,catcromar@gmail.com';
 $cc = 'andrewmcromar@gmail.com';
