67d0cdaf306d4b1529cc7f13b15e600e8f65e0a7
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-01-26 16:39:20 -0800

    feat(wishlist): close for maintenance

--- wishlist/index.php
+++ wishlist/index.php
@@ -1,4 +1,5 @@
-<meta charset="UTF-8">
+<!-- <meta charset="UTF-8">
 <meta http-equiv="refresh" content="0; url=./pages/dashboard.php">
 <title>Redirecting...</title>
-<p>If you are not redirected, <a href="./pages/dashboard.php">click here</a>.</p>
+<p>If you are not redirected, <a href="./pages/dashboard.php">click here</a>.</p> -->
+<h1>Site Closed for Maintenance</h1>
